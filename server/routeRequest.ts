39abd93578fbf8ea4589edb42d8009051c83bb37
gnajor <lejonet04@gmail.com>
2025-03-13 14:17:27 +0100

    server now works AKA localhost:8000

--- server/routeRequest.ts
+++ server/routeRequest.ts
@@ -1,15 +1,17 @@
 import { serveDir } from "jsr:@std/http/file-server";
 import { handleRequests } from "../api/handleRequests.ts"
 
-export function serveFileOrDir(request: Request){
+export function routeRequest(request: Request){
     const pathname = new URL(request.url).pathname;
 
-    if(pathname.startsWith("/api/")){
+/*     if(pathname.startsWith("/api/")){
         return handleRequests(request);
-    }
+    } */
 
     return serveDir(request, {
         fsRoot: "public",
         urlRoot: ""
     });
 }
+
+Deno.serve(routeRequest);
