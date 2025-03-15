68eb6273c61f8f795bcb5ed1c2af67795ec5a5af
gnajor <lejonet04@gmail.com>
2025-03-15 20:18:25 +0100

    done with the homepage

--- public/pages/homePage/card.js
+++ public/pages/homePage/card.js
@@ -1,14 +1,16 @@
 export function renderHomePageCard(parentId, data){
     const parent = document.querySelector(parentId);
-    parent.innerHTML += `<div id="card">
-                            <div class="line"></div>
-                            <div class="text-container">
-                                <h2>${data.header}</h2>
-                                <p>${data.text}</p>
-                                <a class="card-link">
-                                    <span>${data.link}</span>
-                                    <img src="icon"></img>
-                                </a>
+    parent.innerHTML += `<div class="card">
+                            <div class="text-line-container">
+                                <div class="line"></div>
+                                <div class="text-container">
+                                    <h2>${data.header}</h2>
+                                    <p>${data.text}</p>
+                                    <a class="card-link">
+                                        <span>${data.link}</span>
+                                        <img src="../../media/icons/popout.svg"></img>
+                                    </a>
+                                </div>
                             </div>
                             <img src="${data.img}">
                         </div>`;
